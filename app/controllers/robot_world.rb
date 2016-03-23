0f616577097b57974ebf8ac3781fe3dca5a53e40
Jared Roth <jared.roth@gmail.com>
2016-03-23 09:49:12 -0600

    Basically finished Robot CRUD

--- app/controllers/robot_world.rb
+++ app/controllers/robot_world.rb
@@ -1,3 +1,5 @@
+require 'models/robots'
+
 class RobotWorld < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   set :method_override, true
@@ -7,31 +9,41 @@ class RobotWorld < Sinatra::Base
   end
 
   get '/robots'do
-  @robots = robots.all
+    @robots = robots.all
     erb :index
   end
 
-  get '/robots/:name' do
-    erb :show
-  end
-
-  get 'robots/new' do
+  get '/robots/new' do
     erb :new
   end
 
+  get '/robots/:name' do |name|
+    @robot = robots.find(name)
+    erb :show
+  end
+
   get '/robots/:name/edit' do |name|
+    @robot = robots.find(name)
     erb :edit
   end
 
-  post 'robots' do
+  post '/robots' do
+    robots.create(params[:robot])
     redirect '/robots'
   end
 
-  put 'robots/:name' do |name|
-    redirect '/robots'+name
+  put '/robots/:name' do |name|
+    robots.update(name, params[:robot])
+    redirect '/robots/'+name
   end
 
-  delete 'robots/:name' do |name|
+  delete '/robots/:name' do |name|
+    robots.delete(name)
     redirect '/robots'
+  end
+
+  def robots
+    database = YAML::Store.new('db/robot_world')
+    @robots ||= Robots.new(database)
   end
 end
